46f461ce62fbc4bc88fce109f2e954e779439fd0
Andy <andreslpo06@gmail.com>
2026-03-05 13:52:43 +0100

    Add iterative version of algorithm

---
+++ iterativo.cpp
@@ -0,0 +1,23 @@
+#include "iterativo.hpp"
+
+Solucion iterativo(string A, set<char> S)
+{
+    int n = A.length();
+    Solucion solucion = {0, set<int>()};
+
+    // CASO BASE: Si la longitud de la cadena es menor que 3, no se pueden formar subcadenas válidas
+    if (n < 3)
+        return solucion;
+
+    // RECORRIDO: Verificar todas las subcadenas de longitud 3
+    for (int i = 0; i <= n - 3; i++)
+    {
+        if (esValida(A, i, S))
+        {
+            solucion.n_subcadenas++;
+            solucion.posiciones.insert(i);
+        }
+    }
+
+    return solucion;
+}
---
+++ iterativo.hpp
@@ -0,0 +1,8 @@
+#ifndef ITERATIVO_HPP
+#define ITERATIVO_HPP
+
+#include "common.hpp"
+
+Solucion iterativo(string A, set<char> S);
+
+#endif // ITERATIVO_HPP
